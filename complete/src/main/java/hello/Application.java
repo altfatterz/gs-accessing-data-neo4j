d7aa32e0b26892e48d31eaf8b8b4d241102c8b26
Zoltan Altfatter <altfatterz@gmail.com>
2016-09-20 21:24:33 +0200

    removed unnecessary @EnableNeo4jRepositories, put back the default constructor needed when connecting to external neo4j server

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -14,7 +14,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 
 @SpringBootApplication
-@EnableNeo4jRepositories
 public class Application {
 
 	private final static Logger log = LoggerFactory.getLogger(Application.class);
